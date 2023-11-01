0ca5f1f2c16c431b52074daec91750b240345925
John Alexis Guerra Gómez <john.guerra@gmail.com>
2023-11-01 14:55:05 -0700

    Adding react as the front end

---
+++ front/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div>
+      <h1>¡Hola mundo!</h1>
+      <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo, libero ipsa. Rem, eum. Officia iste sint inventore ipsa esse! Autem numquam deleniti harum a soluta, minima maiores mollitia et dolorum?</div>
+    </div>
+  );
+}
