8f64ca8d9763e4bacb3e3cbdbd22b6016a0380ff
John Alexis Guerra Gómez <john.guerra@gmail.com>
2023-11-01 14:53:10 -0700

    Adding react as the front end

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 import express from "express";
-import path from "path";
+import path, { dirname } from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
+import { fileURLToPath } from "url";
 
 import indexRouter from "./routes/index.js";
 
-// Copilot generated
-import { fileURLToPath } from "url";
-import { dirname } from "path";
 
+// ES6 modules don't have __dirname
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-// /Copilot generated
 
 let app = express();
 
@@ -19,7 +17,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "front", "dist")));
 
 app.use("/", indexRouter);
 
